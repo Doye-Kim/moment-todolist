93a4dce24859e2786b53e7537010516a750418cc
도예 <kmdy125@gmail.com>
2023-10-10 16:32:25 +0900

    Design: 전체 css 수정

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,17 @@
-const clock = document.querySelector('h2#clock');
+const clock = document.querySelector("h2#clock");
+const today = document.querySelector("h2#date");
 
-function getClock(){
-	const date = new Date();
-	const hours = String(date.getHours()).padStart(2, '0');
-	const minutes = String(date.getMinutes()).padStart(2, '0');
-	const seconds = String(date.getSeconds()).padStart(2, '0');
-	clock.innerText = `${hours}:${minutes}:${seconds}`;
+function getClock() {
+  const date = new Date();
+  const year = String(date.getFullYear());
+  const month = String(date.getMonth()).padStart(2, "0");
+  const day = String(date.getDay()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  today.innerText = `${year}년 ${month}월 ${day}일`;
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock, 1000);
--- js/greeting.js
+++ js/greeting.js
@@ -1,32 +1,28 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = document.querySelector('#login-form input');
-const greeting = document.querySelector('#greeting');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = 'hidden';
-const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function loginSubmit(event){
-	event.preventDefault();
-	const username = loginInput.value;
-	loginForm.classList.add(HIDDEN_CLASSNAME);
-	localStorage.setItem(USERNAME_KEY, username);
-	paintGreeting(username);
+function loginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-	loginForm.classList.remove(HIDDEN_CLASSNAME);
-	loginForm.addEventListener('submit', loginSubmit);
-}
-else {
-	paintGreeting(savedUsername);
-}
-
-function paintGreeting(username){
-	greeting.classList.remove(HIDDEN_CLASSNAME);
-	greeting.innerText = `Hello, ${username}`;
-
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", loginSubmit);
+} else {
+  paintGreeting(savedUsername);
 }
 
-
+function paintGreeting(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `${username}'s ToDo`;
+}
